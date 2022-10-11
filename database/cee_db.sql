d73869c11d35a0dcaba8cb51a493779450c90ee7
Upal Mondal <114487187+upal48@users.noreply.github.com>
2022-10-12 02:33:45 +0600

    Database

---
+++ database/cee_db.sql
@@ -0,0 +1,98 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 11, 2022 at 09:54 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cee_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_acc`
+--
+
+CREATE TABLE `admin_acc` (
+  `admin_id` int(11) NOT NULL,
+  `admin_user` varchar(1000) NOT NULL,
+  `admin_pass` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin_acc`
+--
+
+INSERT INTO `admin_acc` (`admin_id`, `admin_user`, `admin_pass`) VALUES
+(1, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_tbl`
+--
+
+CREATE TABLE `course_tbl` (
+  `cou_id` int(11) NOT NULL,
+  `cou_name` varchar(1000) NOT NULL,
+  `cou_created` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `course_tbl`
+--
+
+INSERT INTO `course_tbl` (`cou_id`, `cou_name`, `cou_created`) VALUES
+(67, 'WEB PROGRAMMING', '2022-10-11 19:05:23'),
+(68, 'WEB', '2022-10-11 19:53:49');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_acc`
+--
+ALTER TABLE `admin_acc`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `course_tbl`
+--
+ALTER TABLE `course_tbl`
+  ADD PRIMARY KEY (`cou_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin_acc`
+--
+ALTER TABLE `admin_acc`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `course_tbl`
+--
+ALTER TABLE `course_tbl`
+  MODIFY `cou_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
